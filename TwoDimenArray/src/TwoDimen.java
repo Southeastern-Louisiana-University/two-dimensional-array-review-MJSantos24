17d153045b13d3ff679d194977767d4bc30fe346
MJ Santos <micaellajhoyce.santos@selu.edu>
2026-02-13 21:57:24 -0600

    two dimen

---
+++ TwoDimenArray/src/TwoDimen.java
@@ -0,0 +1,26 @@
+public class TwoDimen {
+    //int[][] matrix = new int[10][10];
+    //java.util.Scanner input = new java.util.Scanner(System.in);
+    //System.out.println("Enter" + matrix.length + "rows and " + matrix[0].length + "columns: ");
+    //for (int row = 0; column < matrix(row).length; column++){
+      //  matrix(row)(column) = input.nextInt();
+  //  }
+//}
+
+// printing arrays
+// for (int row = 0; row <length; row++){
+// for (int column = 0); column < matrix(row).length; column++){
+//   System.out.println(matrix(row)(column));
+//}
+//System.out.println();
+//}
+//}
+
+// initialize total
+
+/*int total = 0;
+    for (int row = 0; row < matrix.length; row++){
+        for (int column = 0); column < matrix(row).length; column++){
+    total += matrix(row(column));
+     //   }
+    //}
